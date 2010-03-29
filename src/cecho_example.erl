76bd46cb0a8c839bd818e751fb61930b26bafbef
Mazen Harake <mazen.harake@gmail.com>
2010-03-29 11:40:44 +0200

    Updated the README file a bit to conform with the new input functionality. Added a new example.

--- src/cecho_example.erl
+++ src/cecho_example.erl
@@ -189,7 +189,6 @@ input_counter(N) ->
     timer:sleep(100),
     input_counter(N+1).
 
-
 %%
 %% cursmove - move the '@' around the screen with the arrow keys. 'q' to quit.
 %%
@@ -224,3 +223,71 @@ mv(OffsetY, OffsetX) ->
     FinalX = CX+(OffsetX),
     cecho:mvaddch(FinalY,FinalX,$@),
     cecho:move(FinalY, FinalX).
+
+%%
+%% helloworld - bounce "Hello World!" on the end of the screen
+%%
+helloworld() ->
+    %% Start application
+    application:start(cecho),
+    %% Set attributes
+    cecho:cbreak(),
+    cecho:noecho(),
+    cecho:curs_set(?ceCURS_INVISIBLE),
+    %% Write initial string...
+    cecho:mvaddstr(0, 0, "Hello World!"),
+    cecho:refresh(),
+    %% Start the process that will "move" the string
+    Mover = spawn(fun() -> mvhello() end),
+    ctrl(Mover).
+
+ctrl(Mover) ->
+    %% get key-input
+    C = cecho:getch(),
+    case C of
+	$q -> 
+	    %% If we get a 'q' then exit the mover and stop cecho
+	    exit(Mover, normal),
+	    application:stop(cecho),
+	    erlang:halt();
+	_ ->
+	    %% ignore anything else
+	    ctrl(Mover)
+    end.
+
+%% start the mover
+mvhello() -> mvhello(0, 0, 1, 1).
+%% take previous pos and direction and print out new string
+mvhello(PrevY, PrevX, DirY, DirX) ->
+    %% "erase" previous position
+    cecho:mvaddstr(PrevY, PrevX, "            "),
+    %% calculate new position and direction
+    {NewY, NewX, NewDirY, NewDirX} =
+	calc_new_pos(PrevY, PrevX, DirY, DirX),
+    %% "move" the text to new position
+    cecho:mvaddstr(NewY, NewX, "Hello World!"),
+    %% update the screen to show the change
+    cecho:refresh(),
+    %% do it again!
+    timer:sleep(50),
+    mvhello(NewY, NewX, NewDirY, NewDirX).
+
+calc_new_pos(Py, Px, Dy, Dx) ->
+    %% get max coords of the screen
+    {My, Mx} = cecho:getmaxyx(),
+    %% calc new vertical position and new direction
+    {NewPy, NewDy} =
+	if (Py+(Dy) >= My) orelse (Py+(Dy) < 0) ->
+		{Py+(Dy*-1), Dy*-1};
+	   true ->
+		{Py+(Dy), Dy}
+	end,
+    %% calc new horizontal position and new direction
+    %% take string length into account
+    {NewPx, NewDx} =
+	if (Px+(Dx)+12 >= Mx) orelse (Px+(Dx) < 0) ->
+		{Px+(Dx*-1), Dx*-1};
+	   true ->
+		{Px+(Dx), Dx}
+	end,
+    {NewPy, NewPx, NewDy, NewDx}.
